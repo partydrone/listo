8972ea06db776603cc13a20b47f374b91ded90fc
Andrew Porter <partydrone@icloud.com>
2023-01-14 14:22:33 -0600

    Add healthcheck endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/up/", to: "up#index", as: :up
+  get "/up/databases", to: "up#databases", as: :up_databases
 end
